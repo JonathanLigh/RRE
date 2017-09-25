f746fbe5f385fc38085a88d23e4111381763e0ac
Seva Luchianov <seva.luchianov@gmail.com>
2017-09-25 12:20:37 -0400

    Add Apache HTTP Libraries for webCrawler
    Signed-off-by: Seva Luchianov <seva.luchianov@gmail.com>

--- src/webCrawler/Crawler.java
+++ src/webCrawler/Crawler.java
@@ -1,9 +1,50 @@
 package webCrawler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class Crawler {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        // search things
+      try {
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpGet getRequest = new HttpGet(
+            "http://localhost:8080/RESTfulExample/json/product/get");
+        getRequest.addHeader("accept", "application/json");
+
+        HttpResponse response = httpClient.execute(getRequest);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+               + response.getStatusLine().getStatusCode());
+        }
+
+        BufferedReader br = new BufferedReader(
+                         new InputStreamReader((response.getEntity().getContent())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        httpClient.getConnectionManager().shutdown();
+
+      } catch (ClientProtocolException e) {
+
+        e.printStackTrace();
+
+      } catch (IOException e) {
+
+        e.printStackTrace();
+      }
+
     }
+
 }
