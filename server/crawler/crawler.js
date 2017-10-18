d75e23be76cada61ceb74998a42ad6da81b9353b
Seva Luchianov <seva.luchianov@gmail.com>
2017-10-18 19:37:49 -0400

    crawler working
    Signed-off-by: Seva Luchianov <seva.luchianov@gmail.com>

--- server/crawler/crawler.js
+++ server/crawler/crawler.js
@@ -1,96 +1,92 @@
 var https = require('https');
-var querystring = require('querystring')
+var fs = require('fs');
 
-var client_id = 'KWV3tzhr2r5XGA'; // app clientID
-var secret_key = 'WEfEcM-72OLkL69yRDmLgrTmQUU'; // app clientSecret
+var batchSize = 10;
+var state;
+
+function getReddits(after) {
+    return new Promise(function(resolve, reject) {
+        function get_json(url, callback) {
+            https.get(url, function(res) {
+                var body = '';
+                res.on('data', function(chunk) {
+                    body += chunk;
+                });
+
+                res.on('end', function() {
+                    var response = JSON.parse(body);
+                    callback(response);
+                });
+            });
+        }
+
+        function buildURL(after) {
+            var url = "https://www.reddit.com/reddits.json";
+            if (!!batchSize) {
+                url += "?limit=" + batchSize;
+            }
+            if (!!after) {
+                url += "&after=" + after;
+            }
+            console.log(url);
+            return url;
+        }
+
+        function parseSubreddit(subreddit) {
+            var subredditData = {
+                url: subreddit.url,
+                genre: subreddit.audience_target,
+                total_subscribers: subreddit.subscribers,
+
+            }
+
+            // put parsed stuff in db
+            console.log(subredditData.url);
+
+            return subredditData;
+        }
+
+        get_json(buildURL(after, batchSize), function(response) {
+            var parsedData = {
+                parsedSubredditData: []
+            };
+
+            var subreddit;
+            for (subreddit in response.data.children) {
+                parsedData.parsedSubredditData.push(
+                    parseSubreddit(
+                        response.data.children[subreddit].data
+                    )
+                );
+            }
+            parsedData.after = response.data.after;
+            resolve(parsedData);
+        });
+    });
+}
+
+function continueSearch(after) {
+    getReddits(after).then(
+        function(parsedData) {
+            setTimeout(function() {
+                continueSearch(parsedData.after)
+            }, 1000);
+        },
+        function(error) {
+            console.log(error);
+        });
+}
 
 module.exports = {
-    getAuthToken: function() {
-        var postData = { //the POST request's body data
-            grant_type: 'client_credentials',
-            username: client_id,
-            password: secret_key
-        };
-
-        var postBody = querystring.stringify(postData);
-
-        var options = {
-            host: 'www.reddit.com',
-            path: '/api/v1/access_token',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-                    //'Content-Length': postBody.length
-            },
-        };
-
-        var req = https.request(options, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-            // Buffer the body entirely for processing as a whole.
-            var bodyChunks = [];
-            res.on('data', function(chunk) {
-                // You can process streamed parts here...
-                bodyChunks.push(chunk);
-            }).on('end', function() {
-                var body = Buffer.concat(bodyChunks);
-                console.log('BODY: ' + body);
-                // ...and/or process the entire body here.
-            })
-        });
-
-        req.on('error', function(e) {
-            console.log('ERROR: ' + e.message);
-        });
-
-        req.write(postBody);
-        req.end();
-    },
-    httpGet: function() {
-        var token_ // variable will store the token
-        var tokenUrl = "https://www.reddit.com/api/v1/access_token"; // Your application token endpoint
-        var request = new XMLHttpRequest();
-
-        function getToken(url, clientID, clientSecret) {
-            var key;
-            request.open("POST", url, true);
-            request.setRequestHeader("Content-type", "application/json");
-            request.send("grant_type=client_credentials&client_id=" +
-                clientID + "&" + "client_secret=" + clientSecret); // specify the credentials to receive the token on request
-            request.onreadystatechange = function() {
-                if (request.readyState == request.DONE) {
-                    var response = request.responseText;
-                    var obj = JSON.parse(response);
-                    console.log(obj);
-                    key = obj.access_token; //store the value of the accesstoken
-                    token_ = key; // store token in your global variable "token_" or you could simply return the value of the access token from the function
-                }
+    crawl: function() {
+        fs.readFile('state.json', (err, data) => {
+            if (err) {
+                throw err;
             }
-        }
-        // Get the token
-        getToken(tokenUrl, client_id, secret_key);
-        console.log(token_);
-    },
-    /*testXHR: function() {
-        xhr.get({
-            url: "https://www.reddit.com/r/overwatch/about"
-        }, function(err, resp, body) {
-            var toReturn;
-            toReturn.test = "ABC";
-            toReturn.x = resp;
-            toReturn.y = body;
-            toReturn.e = err;
-            return toReturn;
+            state = JSON.parse(data);
+            continueSearch();
         });
-        return {
-            idk: "123"
-        };
-    },*/
-    test: function() {
-        console.log('hello');
-    },
-    simpleValue: 'also works'
+    }
 };
 
 require('make-runnable');
