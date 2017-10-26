7506f260943fb57059745b784c3c50fe84329a76
Seva Luchianov <seva.luchianov@gmail.com>
2017-10-26 19:34:24 -0400

    adding tests for crawler
    Signed-off-by: Seva Luchianov <seva.luchianov@gmail.com>

--- server/test/crawler/crawler.test.js
+++ server/test/crawler/crawler.test.js
@@ -3,6 +3,8 @@ const chai = require('chai');
 const spies = require('chai-spies');
 chai.use(spies);
 const expect = chai.expect;
+var crawler = require('../../crawler/crawler');
+var fs = require('fs');
 
 /*
 For the first few functions I wrote a bunch of example possible tests
@@ -15,89 +17,279 @@ refactoring now that we have it functioning. -Jonathan
 */
 
 describe('Testing buildURL', () => {
+    it('buildURL_noAfter', () => {
+        // Given
 
-  it('buildURL correctly builds the URL', () => {
+        // When
+        var url = crawler._buildURL();
 
-  });
+        // Then
+        expect(url, "https://www.reddit.com/reddits.json?limit=1");
+    });
 
-  it('buildURL does this if no batchsize', () => {
+    it('buildURL_emptyStringAfter', () => {
+        // Given
+        var after = ""
 
-  });
+        // When
+        var url = crawler._buildURL(after);
 
-  it('buildURL does this if no argument specified', () => {
+        // Then
+        expect(url, "https://www.reddit.com/reddits.json?limit=1");
+    });
 
-  });
+    it('buildURL_validAfter', () => {
+        // Given
+        var after = "test"
 
-  it('buildURL throws an error when ...', () => {
+        // When
+        var url = crawler._buildURL(after);
 
-  });
-//etc...
-});
-
-describe('Testing parseSubreddit', () => {
-
-  it('parseSubreddit outputs expected value', () => {
-
-  });
-
-  it('parseSubreddit throws an error when expected', () => {
-
-  });
-
-  it('parseSubreddit does this if the subreddit does not exists', () => {
-
-  });
-
+        // Then
+        expect(url, "https://www.reddit.com/reddits.json?limit=1&after=test");
+    });
 });
 
 describe('Testing writeSubreddit', () => {
+    beforeEach(() => {
+        fs.mkdirSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  it('writeSubreddit correctly writes out the subreddit with expected values', () => {
+    afterEach(() => {
+        fs.unlinkSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  });
+    it('whenNewData_writeSubreddit_fileCreated', () => {
+        // Given
+        var fileName = "a";
+        var data = {
+            "test": "test"
+        }
 
-  it('writeSubreddit throws an error when expected', () => {
+        // When
+        crawler._writeSubreddit(fileName, data);
 
-  });
+        // Then
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + fileName + ".json"), true);
+        var readData = fs.readFileSync(parsedSubredditFolder(true) + fileName + ".json");
+        expect(readData.test, data.test);
+    });
 
+    it('whenExistingData_writeSubreddit_fileUpdated', () => {
+        // Given
+        var fileName = "a";
+        var data = {
+            "test": "test"
+        };
+        crawler._writeSubreddit(fileName, data);
+        var updateData = {
+            "test": "test2"
+        };
+
+        // When
+        crawler._writeSubreddit(fileName, updateData);
+
+        // Then
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + fileName + ".json"), true);
+        var readData = fs.readFileSync(parsedSubredditFolder(true) + fileName + ".json");
+        expect(readData.test, data.test);
+    });
+
+});
+
+describe('Testing updateTag', () => {
+    it('whenTagsEmpty_updateTag_tagCreated', () => {
+        // Given
+        var subredditData = {
+            tags: []
+        };
+        var newTag = {
+            tag: "tag",
+            mentionDistance: 0
+        };
+        var depth = 0;
+
+        // When
+        var updated = crawler._updateTag(subredditData, newTag, depth);
+
+        // Then
+        expect(updated, true);
+        expect(subredditData.tags.length, 1);
+        expect(subredditData.tags[0].tag, newTag.tag);
+    });
+
+    it('whenTagExists_updateTag_closerDistance_tagUpdated', () => {
+        // Given
+        var subredditData = {
+            tags: [{
+                tag: "tag",
+                mentionDistance: 1
+            }]
+        };
+        var newTag = {
+            tag: "tag",
+            mentionDistance: 0
+        };
+        var depth = 0;
+
+        // When
+        var updated = updateTag(subredditData, newTag, depth);
+
+        // Then
+        expect(updated, true);
+        expect(subredditData.tags.length, 1);
+        expect(subredditData.tags[0].tag, newTag.tag);
+        expect(subredditData.tags[0].mentionDistance, depth);
+    });
+
+    it('whenTagExists_updateTag_fartherDistance_tagNotUpdated', () => {
+        // Given
+        var subredditData = {
+            tags: [{
+                tag: "tag",
+                mentionDistance: 0
+            }]
+        };
+        var newTag = {
+            tag: "tag",
+            mentionDistance: 0
+        };
+        var depth = 1;
+
+        // When
+        var updated = updateTag(subredditData, newTag, depth);
+
+        // Then
+        expect(updated, false);
+        expect(subredditData.tags.length, 1);
+        expect(subredditData.tags[0].mentionDistance, 0);
+    });
 });
 
 describe('Testing propogateSubredditData', () => {
+    beforeEach(() => {
+        fs.mkdirSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  it('propogateSubredditData outputs expected value', () => {
+    afterEach(() => {
+        fs.unlinkSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  });
+    it('whenNewSubreddit_propogateSubredditData_oneRelation_createsRelation_createsTags', () => {
+        // Given
+        var subredditURL = "/r/child";
+        var parentSubredditData = {
+            url: "/r/parent",
+            tags: [{
+                tag: "tag"
+                mentionDistance: 0
+            }],
+            name: "test_parent",
+            relatedSubreddits: ["r/child"]
+        };
+        var depth = 1;
+        var searched = [];
 
-  it('propogateSubredditData throws an error when expected', () => {
+        crawler._writeSubreddit("parent", parentSubredditData);
 
-  });
+        // When
+        crawler._propagateSubredditData(subredditURL, parentSubredditData, depth, searched);
 
-  it('propogateSubredditData does this if the subreddit does not exists', () => {
+        // Then
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + "child.json"), true);
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + "parent.json"), true);
 
-  });
+        var childData = fs.readFileSync(parsedSubredditFolder(true) + "child.json");
+        var parentData = fs.readFileSync(parsedSubredditFolder(true) + "parent.json");
 
+        expect(childData.relatedSubreddits.length, 1);
+        expect(childData.relatedSubreddits[0], "r/parent");
+        expect(childData.tags.length, 1);
+        expect(childData.tags[0].tag, "tag");
+        expect(childData.tags[0].mentionDistance, 1);
+
+        expect(parentData.relatedSubreddits.length, 1);
+        expect(parentData.relatedSubreddits[0], "r/child");
+        expect(parentData.tags.length, 1);
+        expect(parentData.tags[0].tag, "tag");
+        expect(parentData.tags[0].mentionDistance, 0);
+    });
+
+    it('whenExistingSubredditWithTags_propogateSubredditData_oneRelation_createsRelation_updatesAndPropagatesTags', () => {
+        // Given
+        var subredditURL = "/r/existing";
+        var parentSubredditData = {
+            url: "/r/parent",
+            tags: [{
+                tag: "parentTag"
+                mentionDistance: 0
+            }],
+            name: "test_parent",
+            relatedSubreddits: ["r/existing"]
+        };
+        var depth = 1;
+        var searched = [];
+
+        crawler._writeSubreddit("parent", parentSubredditData);
+
+        var existingSubredditData = {
+            url: subredditURL,
+            tags: [{
+                tag: "existingTag"
+                mentionDistance: 0
+            }],
+            name: "test_existing",
+            relatedSubreddits: []
+        };
+
+        crawler._writeSubreddit("existing", existingSubredditData);
+
+        // When
+        crawler._propagateSubredditData(subredditURL, parentSubredditData, depth, searched);
+
+        // Then
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + "existing.json"), true);
+        expect(fs.existsSync(crawler.parsedSubredditFolder(true) + "parent.json"), true);
+
+        var existingData = fs.readFileSync(parsedSubredditFolder(true) + "existing.json");
+        var parentData = fs.readFileSync(parsedSubredditFolder(true) + "parent.json");
+
+        expect(existingData.relatedSubreddits.length, 1);
+        expect(existingData.relatedSubreddits[0], "r/parent");
+        expect(existingData.tags.length, 2);
+        expect(existingData.tags[0].tag, "existingTag");
+        expect(existingData.tags[0].mentionDistance, 0);
+        expect(existingData.tags[0].tag, "parentTag");
+        expect(existingData.tags[0].mentionDistance, 1);
+
+        expect(parentData.relatedSubreddits.length, 1);
+        expect(parentData.relatedSubreddits[0], "r/existing");
+        expect(parentData.tags.length, 2);
+        expect(parentData.tags[0].tag, "parentTag");
+        expect(parentData.tags[0].mentionDistance, 0);
+        expect(parentData.tags[0].tag, "existingTag");
+        expect(parentData.tags[0].mentionDistance, 2);
+    });
 });
 
-describe('Testing updateTags', () => {
+describe('Testing parseSubreddit', () => {
+    beforeEach(() => {
+        fs.mkdirSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  it('updateTags outputs expected value', () => {
+    afterEach(() => {
+        fs.unlinkSync("../../crawler/" + crawler.parsedSubredditFolder(true));
+    });
 
-  });
+    it('whenCreatingSubreddit_parseSubreddit_subredditCreated', () => {
 
-  it('updateTags throws an error when expected', () => {
+    });
 
-  });
+    it('whenUpdatingSubreddit_parseSubreddit_subredditUpdated', () => {
 
-});
+    });
 
-describe('Testing loadStateJSON', () => {
+    it('whenUpdatingSubredditWithRelations_parseSubreddit_subredditAndRelationsUpdated', () => {
 
-  it('loadStateJSON outputs expected value', () => {
-
-  });
-
-  it('loadStateJSON throws an error when expected', () => {
-
-  });
-
+    });
 });
