0209c60752018599cd5bd985fc208097dc35ad6e
Seva Luchianov <seva.luchianov@gmail.com>
2017-10-26 22:29:40 -0400

    Fuck DS Store btw
    Signed-off-by: Seva Luchianov <seva.luchianov@gmail.com>

--- server/test/crawler/recommend.test.js
+++ server/test/crawler/recommend.test.js
@@ -3,7 +3,26 @@ const spies = require('chai-spies');
 chai.use(spies);
 const equalTo = chai.assert.strictEqual;
 const recommend = require('../../crawler/recommend.js')
+var fs = require('fs');
 
+var testDir = crawler.parsedSubredditDir(true);
+
+var deleteDir = function(path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index) {
+            var curPath = path + "/" + file;
+            fs.unlinkSync(curPath);
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+var createCleanDir = function(path) {
+    if (fs.existsSync(path)) {
+        deleteDir(path);
+    }
+    fs.mkdirSync(testDir);
+};
 
 describe('Testing buildURL', () => {
 
