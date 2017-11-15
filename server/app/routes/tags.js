abad95dda74d59c9316e0e837be949a78d95b1e6
Jonathan Ligh <jonathankligh@gmail.com>
2017-11-15 16:10:14 -0500

    removed all tag routes we will not use

--- server/app/routes/tags.js
+++ server/app/routes/tags.js
@@ -3,20 +3,6 @@ const router = express.Router();
 const models = require('../../db/models');
 const Tag = models.Tag;
 
-/*
-this function finds the corresponding account object
-and sets it to the requestedAccount variable on req
-*/
-router.param('tagId', function(req, res, next, id) {
-    Tag.findOne({_id: id}).exec()
-        .then(tag => {
-            req.requestedTag = tag;
-            next();
-            return null; //silences a bluebird warning about promises inside of next
-        })
-        .catch(next);
-});
-
 // responds with all tags in the database
 // we will need to be able to serve this up to users so they can add existing
 // tags to their prefered tags.
@@ -27,19 +13,6 @@ router.get('/', function (req, res, next) {
     res.status(200).json(tags);
   })
   .catch(next);
-});
-
-router.get('/name/:tagName', function (req, res, next) {
-  Tag.findOne({
-    name: req.params.tagName
-  }).exec()
-  .then(tag => res.json(tag))
-  .catch(next);
-})
-
-router.get('/:tagId', /*add access checking functions here*/ function (req, res, next){
-  const status = (req.requestedTag) ? 200 : 404;
-    res.status(status).json(req.requestedTag);
 });
 
 module.exports = router;
