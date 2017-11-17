4171870e27627bd3d9429526eea0b4d517f8553e
alexmf219 <alexmf219@gmail.com>
2017-11-16 19:35:45 -0500

    jonathan ligh was here

--- server/test/db/subreddit.model.test.js
+++ server/test/db/subreddit.model.test.js
@@ -12,124 +12,12 @@ const utils = require('../utils');
 
 
 describe('Subreddit Model', () => {
-    let subreddit;
-    const unfoundSubreddit = {
-        url: '/r/SRTestNotFound',
-        tags: [{
-            name: 'tag3',
-            distance: 3
-        }, {
-            name: 'tag2',
-            distance: 2
-        }],
-        numSubscribers: 1004,
-        _relatedSubreddits: []
-    },
-    existingSubreddit = {
-        url: '/r/SRTest1',
-        tags: [{
-            name: 'tag1',
-            distance: 3
-        }, {
-            name: 'tag2',
-            distance: 7
-        }, {
-            name: 'tag3',
-            distance: 2
-        }, {
-            name: 'tag4',
-            distance: 2
-        }, {
-            name: 'tag5',
-            distance: 12
-        }],
-        numSubscribers: 100,
-        _relatedSubreddits: []
-    };
+    
 
-    // Before Each Test
-    before(done => {
-        Subreddit.create({
-            url: '/r/SRTest1',
-            tags: [{
-                name: 'tag1',
-                distance: 3
-            }, {
-                name: 'tag2',
-                distance: 7
-            }, {
-                name: 'tag3',
-                distance: 2
-            }, {
-                name: 'tag4',
-                distance: 2
-            }, {
-                name: 'tag5',
-                distance: 12
-            }],
-            numSubscribers: 100,
-            _relatedSubreddits: []
-        }).then(() => {
-            return Subreddit.create({
-                url: '/r/SRTest2',
-                tags: [{
-                    name: 'tag1',
-                    distance: 7
-                }, {
-                    name: 'tag2',
-                    distance: 5
-                }, {
-                    name: 'tag3',
-                    distance: 9
-                }, {
-                    name: 'tag4',
-                    distance: 10
-                }, {
-                    name: 'tag5',
-                    distance: 2
-                }],
-                numSubscribers: 1000,
-                _relatedSubreddits: []
-            });
-        }).then(() => {
-            done();
-        }).catch(done);
-    });
+    
 
-    after(done => {
-        Subreddit.remove({}).then(() => {
-            done();
-        })
-    })
 
-    describe('query helpers', () => {
-        describe('Testing getTagsBySubreddits', () => {
-            // it('it gets all tags from all subreddit names in argument', () => {
-            //     Subreddit.find().getTagsBySubreddits(['/r/SRTest1', '/r/SRTest2'], 9).exec(function(err, res) {
-            //         return res;
-            //     }).then(list => {
-            //         var list = list.map(element => element.tags)
-            //             .reduce((a, b) => a.concat(b), []);
-            //         expect(list.length).to.be.equal(10)
-            //     });
-            // });
 
-            // it('it gets no tags if no subreddits are in argument', () => {
-            //     Subreddit.find().getTagsBySubreddits([''], 1).exec(function(err, res) {
-            //         return res;
-            //     }).then(list => {
-            //         var list = list.map(element => element.tags)
-            //             .reduce((a, b) => a.concat(b), []);
-            //         expect(list.length).to.be.equal(0)
-            //     });
-            // });
-        });
 
-        // TO ADD:
-        // describe('Testing getSubredditsByTags', () => {
 
-        // Test Case
-        //   it('will', () => {
-        //   });
-    });
 });
