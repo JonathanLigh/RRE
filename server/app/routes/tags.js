031ee6decd042ee38c4fb8fb27af8383efd7485f
Seva Luchianov <seva.luchianov@gmail.com>
2017-11-25 21:06:04 -0500

    I made the settings overlay work at the expense of opening up the server to cross-origin attacks

--- server/app/routes/tags.js
+++ server/app/routes/tags.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const models = require('../../db/models');
 const Tag = models.Tag;
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // responds with all tags in the database
 // we will need to be able to serve this up to users so they can add existing
 // tags to their prefered tags.
-router.get('/', function (req, res, next) {
-  Tag.find({}).exec()
-  .then(tags => {
-    // sets the status of the response to sucessful and sends the tags
-    res.status(200).json(tags);
-  })
-  .catch(next);
+router.get('/', function(req, res, next) {
+    Tag.find({}).exec().then(tags => {
+        // sets the status of the response to sucessful and sends the tags
+        res.status(200).json(tags);
+    }).catch(next);
 });
 
 module.exports = router;
