f77d92a591e553bb2d6075a68bce545da1e9abac
Jonathan Ligh <jonathankligh@gmail.com>
2018-01-26 11:25:12 -0500

    more model tests

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,8 +2,10 @@ const Subreddit = require('./subreddit');
 const Tag = require('./tag');
 const Relation = require('./relation');
 
-Subreddit.hasMany(Relation, {as: 'relatedSubreddits'})
-Subreddit.hasMany(Tag, {as: 'tags'})
+Subreddit.hasMany(Relation, {as: 'relatedSubreddits',
+                  foreignKey: 'relatedSubreddits'})
+Subreddit.hasMany(Tag, {as: 'tags',
+                  foreignKey: 'tags'})
 
 //  Consolidate all server data models to one export
 module.exports = {
